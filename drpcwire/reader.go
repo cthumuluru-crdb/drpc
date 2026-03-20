4474b7b38be94022c77abfca94ca2d10042bbcd8
Chandra Thumuluru <chandra.thumuluru@cockroachlabs.com>
2026-03-20 18:32:59 +0000

    buffer frames per stream

--- drpcwire/reader.go
+++ drpcwire/reader.go
@@ -19,10 +19,13 @@ type ReaderOptions struct {
 // Reader reconstructs packets from frames read from an io.Reader.
 type Reader struct {
 	opts ReaderOptions
-	r    io.Reader
 	fr   *frameReader
-	id   ID
-	rerr error
+
+	// id, nFrames, and pkt track the current packet being assembled.
+	// Will eventually be replaced with a map from stream ID to in-progress
+	// packet for stream multiplexing.
+	id  ID
+	pkt Packet
 }
 
 // NewReader constructs a Reader to read Packets from the io.Reader.
@@ -44,25 +47,6 @@ func NewReaderWithOptions(r io.Reader, opts ReaderOptions) *Reader {
 	}
 }
 
-// read calls Read on the underlying reader and ensures the the return
-// value is (>0, nil) or (0, err).
-func (r *Reader) read(p []byte) (n int, err error) {
-	for i := 0; i < 100; i++ {
-		if r.rerr != nil {
-			r.rerr, err = nil, r.rerr
-			if err == io.EOF || err == io.ErrUnexpectedEOF {
-				return 0, err
-			}
-			return 0, drpc.ConnectionError.Wrap(err)
-		}
-		n, r.rerr = r.r.Read(p)
-		if n > 0 {
-			return n, nil
-		}
-	}
-	return 0, drpc.InternalError.Wrap(io.ErrNoProgress)
-}
-
 // ReadPacket reads a packet from the io.Reader. It is equivalent to
 // calling ReadPacketUsing(nil).
 func (r *Reader) ReadPacket() (pkt Packet, err error) {
@@ -73,48 +57,42 @@ func (r *Reader) ReadPacket() (pkt Packet, err error) {
 // frames must be monotonically increasing. When a new ID is read, the
 // old data is discarded. This allows for easier asynchronous interrupts.
 // If the amount of data in the Packet becomes too large, an error is
-// returned. The returned packet's Data field is constructed by appending
-// to the provided buf after it has been resliced to be zero length.
+// returned.
 func (r *Reader) ReadPacketUsing(buf []byte) (pkt Packet, err error) {
-	pkt.Data = buf[:0]
-
 	for {
 		fr, err := r.fr.ReadFrame()
 		if err != nil {
 			return Packet{}, err
 		}
 
-		// If any frames are set to control, then the whole packet is
-		// considered to be control.
-		pkt.Control = pkt.Control || fr.Control
-
 		switch {
 		case fr.ID.Less(r.id):
 			return Packet{}, drpc.ProtocolError.New("id monotonicity violation (fr:%v r:%v)", fr.ID, r.id)
 
-		case r.id != fr.ID || pkt.ID == ID{}:
+		// Frame is for a new packet.
+		case r.id != fr.ID || r.pkt.ID == ID{}:
 			r.id = fr.ID
-
-			pkt = Packet{
-				Data:    pkt.Data[:0],
-				ID:      fr.ID,
-				Kind:    fr.Kind,
-				Control: fr.Control,
+			r.pkt = Packet{
+				ID:   fr.ID,
+				Kind: fr.Kind,
+				Data: []byte{},
 			}
 
-		case fr.Kind != pkt.Kind:
-			return Packet{}, drpc.ProtocolError.New("packet kind change (fr:%v pkt:%v)", fr.Kind, pkt.Kind)
+		// Frame kind for the same packet must match.
+		case fr.Kind != r.pkt.Kind:
+			return Packet{}, drpc.ProtocolError.New("packet kind change (fr:%v pkt:%v)", fr.Kind, r.pkt.Kind)
 		}
 
-		pkt.Data = append(pkt.Data, fr.Data...)
+		r.pkt.Data = append(r.pkt.Data, fr.Data...)
+		r.pkt.Control = r.pkt.Control || fr.Control
 
 		switch {
-		case len(pkt.Data) > r.opts.MaximumBufferSize:
-			return Packet{}, drpc.ProtocolError.New("data overflow (len:%v)", len(pkt.Data))
+		case len(r.pkt.Data) > r.opts.MaximumBufferSize:
+			return Packet{}, drpc.ProtocolError.New("data overflow (len:%v)", len(r.pkt.Data))
 
 		case fr.Done:
-			// increment the message id so that we do not accept any frames
-			// with the same id.
+			pkt = r.pkt
+			r.pkt = Packet{}
 			r.id.Message++
 			return pkt, nil
 		}
